6375213a038593891882438158695bec93b8924f
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-11 15:02:34 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+// To make both elements same height. 
 const navUl = document.querySelector('nav ul');
 const navA = document.getElementById('logo-a');
 navA.style.height = navUl.offsetHeight+'px';
+
+// For mobile hamburger
+document.getElementsByClassName("ham")[0].addEventListener("click", (event) => {
+  document.getElementsByClassName("ham")[0].classList.toggle("cross");
+  document.getElementById("line2").classList.toggle("none");
+});

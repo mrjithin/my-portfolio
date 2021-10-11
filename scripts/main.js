38c8553dba8bc00c749ce389959b350325d9c709
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-11 18:18:53 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ const navUl = document.querySelector('nav ul');
 const navA = document.getElementById('logo-a');
 navA.style.height = navUl.offsetHeight+'px';
 
+// Making both same height. 
+const hamCont = document.getElementById('ham-cont');
+hamCont.style.height = navA.offsetHeight+'px';
+
 // For mobile hamburger
 document.getElementsByClassName("ham")[0].addEventListener("click", (event) => {
   document.getElementsByClassName("ham")[0].classList.toggle("cross");

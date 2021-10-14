a897c6ecffb76f85da1c84f5428df58c3195512d
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-14 18:05:55 +0530

    Create scss.js

---
+++ styles/sass/scss.js
@@ -0,0 +1,20 @@
+import sass from "sass";
+import { promisify } from "util";
+import { writeFile } from "fs";
+const sassRenderPromise = promisify(sass.render);
+const writeFilePromise = promisify(writeFile);
+
+async function main(file) {
+  const styleResult = await sassRenderPromise({
+    file: `${process.cwd()}/${file}.scss`,
+    outFile: `../css/{file}.css`,
+    sourceMap: true,
+    sourceMapContents: true,
+    outputStyle: "compressed",
+  });
+  await writeFilePromise("styles.css", styleResult.css, "utf8");
+  await writeFilePromise("styles.css.map", styleResult.map, "utf8");
+}
+main('style');
+main('nav');
+main('chrome');

c0568417d4a8aaef3b03bafae18949ab28d02abb
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-15 17:33:53 +0530

    Update swquery.js

--- scripts/swquery.js
+++ scripts/swquery.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
    function sw() {
-    navigator.serviceWorker.register('/scripts/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);

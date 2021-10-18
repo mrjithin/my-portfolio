05ec38197e5a4547842ca03f08797a7fc9017c54
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-18 20:56:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const filesToCache = [
   '/scripts/main.js'
 ];
 
-let cacheID = 'root-prod-10';
+let cacheID = 'root-prod-11';
 
 self.addEventListener('install', event => {
   console.log('Attempting to install service worker and cache static assets');
@@ -41,6 +41,7 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // CHECKS IF THE REQUEST IS FROM THE SAME ORIGIN AND IF NOT THE FUNCTION RETURNS.
   const url = new URL(event.request.url);
   if (url.origin !== location.origin) {
     return;

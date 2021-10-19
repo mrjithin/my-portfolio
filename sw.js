2e43a7e2ae59053fd515e31bf84702a90880cb77
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-19 16:15:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const filesToCache = [
   '/scripts/main.js'
 ];
 
-let cacheID = 'root-test-174';
+let cacheID = 'root-prod-96';
 
 self.addEventListener('install', event => {
   console.log('Attempting to install service worker and cache static assets');

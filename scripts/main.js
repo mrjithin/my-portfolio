cef506d5e078b5bb6e5b72a4084aa98d64f3f53c
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-19 14:36:16 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 // For mobile hamburger
+const mobNav = document.querySelector("nav#mobile");
+
 document.getElementsByClassName("ham")[0].addEventListener("click", (event) => {
   document.getElementsByClassName("ham")[0].classList.toggle("cross");
   document.getElementById("line2").classList.toggle("none");
+  mobNav.classList.toggle("on");
+  mobNav.classList.toggle("off");
 });

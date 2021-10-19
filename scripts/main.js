88bfa2926ff996757dd107e1b7eecdb46bfd5c72
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-19 16:34:01 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 const mobNav = document.querySelector("nav#mobile");
 const deskNav = document.querySelector("nav.desktop");
 const navColor = window.getComputedStyle(mobNav).getPropertyValue("background-color");
+
 document.getElementsByClassName("ham")[0].addEventListener("click", (event) => {
   document.getElementsByClassName("ham")[0].classList.toggle("cross");
   document.getElementById("line2").classList.toggle("none");
@@ -12,4 +13,16 @@ document.getElementsByClassName("ham")[0].addEventListener("click", (event) => {
   } else {
     deskNav.style.backgroundColor = "";
   }
+  event.stopPropagation();
 });
+
+// To close the navbar on clicking a link. 
+const mobileLis = document.querySelectorAll("nav#mobile a");
+
+Array.from(mobileLis).forEach(link => {
+  link.addEventListener("click", event => {
+    mobNav.classList.toggle("on");
+    mobNav.classList.toggle("off");
+    event.stopPropagation();
+  })
+})

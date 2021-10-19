53ea9422bf6bb8112da9b45f8b98b48436c965fb
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-19 15:22:52 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 // For mobile hamburger
 const mobNav = document.querySelector("nav#mobile");
-
+const deskNav = document.querySelector("nav.desktop");
+const navColor = window.getComputedStyle(mobNav).getPropertyValue("background-color");
 document.getElementsByClassName("ham")[0].addEventListener("click", (event) => {
   document.getElementsByClassName("ham")[0].classList.toggle("cross");
   document.getElementById("line2").classList.toggle("none");
   mobNav.classList.toggle("on");
   mobNav.classList.toggle("off");
+  if(mobNav.className === "on"){
+    deskNav.style.backgroundColor = navColor;
+  } else {
+    deskNav.style.backgroundColor = "";
+  }
 });

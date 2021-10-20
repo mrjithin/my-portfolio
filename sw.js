bb51065d62f8441214f1d17b24e28801efbf7330
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-20 18:35:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ const filesToCache = [
   '/scripts/contact.js/'
 ];
 
-let cacheID = 'root-prod-11';
+let cacheID = 'root-prod-12';
 
 self.addEventListener('install', event => {
   console.log('Attempting to install service worker and cache static assets');

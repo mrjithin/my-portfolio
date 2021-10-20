ea07dab706c871bd3c13f2d20494fe11ab80d898
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-20 21:52:02 +0530

    Update and rename main.js to scroll.js

---
+++ scripts/scroll.js
@@ -0,0 +1,27 @@
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
+
+const skills = document.querySelectorAll(".inner-bar");
+
+function scrolled(item, i) {
+  if(isScrolledIntoView(item)) {
+    item.classList.add("visited");
+    clearInterval(skillObj["skill"+i]);
+  }
+}
+const skillObj = {};
+Array.from(skills).forEach((item, i) => {
+  skillObj["skill"+i] = setInterval(scrolled, 300, item , i);
+})
+
+const date = new Date();
+document.getElementById("year").textContent = date.getFullYear();

e24bea8fdebb5f5a71bb49b8ded34bed7c62b953
Jithin J Joshi <85149870+mrjithin@users.noreply.github.com>
2021-10-28 11:28:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ const filesToCache = [
   '/images/social/codepen.svg',
 ];
 
-const cacheID = 'root-prod-47';
+const cacheID = 'root-prod-48';
 
 self.addEventListener('install', event => {
   console.log('Attempting to install service worker and cache static assets');
